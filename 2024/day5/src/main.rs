4342dc44a65a43cb98c81cc25d77d7027a3ceb60
rayanpiro <rubensangria@gmail.com>
2024-12-06 12:22:08 +0100

    Refactoring day5

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -3,28 +3,6 @@ use std::collections::HashMap;
 type Input = (Restrictions, Vec<Vec<usize>>);
 type Restrictions = HashMap<usize, Vec<usize>>;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct Node {
-    value: usize,
-    childs: Vec<Node>,
-}
-
-impl Node {
-    fn new(updates: &Vec<usize>) -> Self {
-        Self::build_tree(&mut updates.clone())
-    }
-    fn build_tree(updates: &mut Vec<usize>) -> Self {
-        let mut found_nodes = vec![];
-        while updates.len() > 0 {
-            let value = updates.pop().unwrap();
-            let childs = std::mem::take(&mut found_nodes);
-            let node = Self { value, childs };
-            found_nodes.push(node);
-        }
-        found_nodes.pop().unwrap()
-    }
-}
-
 fn read_input() -> Input {
     let content = std::fs::read_to_string("./input.txt").unwrap();
     parse_input(&content)
@@ -55,23 +33,18 @@ fn parse_input(input: &str) -> Input {
     (restrictions, updates)
 }
 
-fn find_error(restrictions: &Restrictions, node: &Node) -> Option<(usize, usize)> {
-    let mut idx_ins = 0;
-    let mut to_inspect = vec![node.to_owned()];
-    while idx_ins < to_inspect.len() {
-        let value = to_inspect[idx_ins].value;
-        for idx_child in 0..to_inspect[idx_ins].childs.len() {
-            let child = &to_inspect[idx_ins].childs[idx_child];
-            if let Some(restr) = restrictions.get(&child.value) {
-                if restr.contains(&value) {
-                    return Some((value, child.value));
-                }
+fn find_error(restrictions: &Restrictions, node: &[usize]) -> Option<(usize, usize)> {
+    for (value_idx, value) in node.iter().enumerate() {
+        for (child_idx, child) in node[value_idx + 1..].iter().enumerate() {
+            let Some(restr) = restrictions.get(&child) else {
+                continue;
+            };
+            if !restr.contains(value) {
+                continue;
             }
-            to_inspect.push(child.clone());
+            return Some((value_idx, child_idx + value_idx + 1));
         }
-        idx_ins += 1;
     }
-
     None
 }
 
@@ -81,8 +54,7 @@ fn part_one() -> usize {
     for update in updates {
         let middle = update.len() / 2;
         let middle_element = update.get(middle).unwrap().to_owned();
-        let node = Node::new(&update);
-        if find_error(&restrictions, &node).is_some() {
+        if find_error(&restrictions, &update).is_some() {
             continue;
         }
         count += middle_element;
@@ -96,14 +68,8 @@ fn part_two() -> usize {
     let mut count = 0usize;
     for update in updates.iter_mut() {
         let mut is_invalid = false;
-        let mut node = Node::new(update);
-        while let Some(error_value) = find_error(&restrictions, &node) {
-            let idx_error = (
-                update.iter().position(|n| n == &error_value.0).unwrap(),
-                update.iter().position(|n| n == &error_value.1).unwrap(),
-            );
+        while let Some(idx_error) = find_error(&restrictions, &update) {
             update.swap(idx_error.0, idx_error.1);
-            node = Node::new(update);
             is_invalid = true;
         }
         if is_invalid {
